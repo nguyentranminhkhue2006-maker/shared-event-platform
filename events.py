49d2304ac594cd92e98f13c0a021da689fbeae90
Becky Nguyen <nguyentranminhkhue2006@gmail.com>
2026-04-15 08:28:57 +0300

    Fix event removal

--- events.py
+++ events.py
@@ -53,6 +53,8 @@ def update_event(event_id, date_time, description, classes):
         db.execute(sql, [event_id,title, value])
 
 def cancel_event(event_id):
+    sql="DELETE FROM event_classes WHERE event_id=?"
+    db.execute(sql, [event_id])
     sql="DELETE FROM events WHERE id=?"
     db.execute(sql, [event_id])
 
